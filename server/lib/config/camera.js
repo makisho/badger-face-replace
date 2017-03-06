2e66c5c22055264f1596daa8389cc8c5488fbf8b
mveritym <mveritym@gmail.com>
2017-03-06 11:39:11 +0000

    Adding config file

---
+++ server/lib/config/camera.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CAM_WIDTH: 1280,
+  CAM_HEIGHT: 720,
+  RESIZE_FACTOR: 5,
+  MASK_IMAGE: 'badger.jpg',
+  ALGORITHM_PATH: './node_modules/opencv/data/haarcascade_frontalface_alt_tree.xml'
+};
