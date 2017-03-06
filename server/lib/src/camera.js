2979c11180ac49a356aaaebe7a7485a23dee8750
mveritym <mveritym@gmail.com>
2017-03-06 11:06:26 +0000

    Moving getImage to camera.js

--- server/lib/src/camera.js
+++ server/lib/src/camera.js
@@ -1,9 +1,14 @@
 var cv = require('opencv');
 
-var makeCamera = (camWidth, camHeight) => {
+var ALGORITHM_PATH = './node_modules/opencv/data/haarcascade_frontalface_alt_tree.xml';
+
+var RESIZE_FACTOR;
+
+var makeCamera = (camWidth, camHeight, resizeFactor) => {
   var camera = new cv.VideoCapture(0);
   camera.setWidth(camWidth);
   camera.setHeight(camHeight);
+  RESIZE_FACTOR = resizeFactor;
   return camera;
 };
 
@@ -17,7 +22,55 @@ var makeMasks = (maskImg, camWidth, maskSizeRatio) => {
   return masks;
 }
 
+function applyMask(mask, camHeight, camWidth, image, x, y) {
+  if ((y + mask.height() < camHeight) && (x + mask.width() < camWidth)) {
+    mask.copyTo(image, x, y);
+    return true;
+  }
+  return false;
+}
+
+function getMask(face, masks) {
+  var maskIndex = Math.floor(face.width * RESIZE_FACTOR / 10) - 1;
+  if (maskIndex < 0) maskIndex = 0;
+  return masks[maskIndex];
+}
+
+var getImage = (camera, masks, camHeight, camWidth, counter, face_backup) => {
+  return new Promise((resolve, reject) => {
+    camera.read(function(err, image) {
+      if (err) reject(err);
+
+      image = image.flip(1);
+
+      var newImage = image.copy();
+      newImage.resize(image.width()/RESIZE_FACTOR, image.height()/RESIZE_FACTOR);
+
+      newImage.detectObject(ALGORITHM_PATH, {}, function(err, faces) {
+        if (err) throw err;
+
+        counter++;
+        if (counter >= 1) {
+          face_backup = faces;
+          counter = 0;
+        }
+        faces = face_backup || faces;
+
+        faces.map(face => {
+          if (face.height > 10) {
+            var mask = getMask(face, masks);
+            applyMask(mask, camHeight, camWidth, image, face.x * RESIZE_FACTOR, face.y * RESIZE_FACTOR);
+          }
+        });
+
+        resolve({image, counter, face_backup});
+      });
+    });
+  });
+}
+
 module.exports = {
   makeCamera,
-  makeMasks
+  makeMasks,
+  getImage
 };
