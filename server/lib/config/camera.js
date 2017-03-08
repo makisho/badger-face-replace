9c06a126f6677a329c8130ac37711cec61d726f4
Etiene Dalcol <dalcol@etiene.net>
2017-03-08 16:13:01 +0000

    Refactor to allow testing perf

--- server/lib/config/camera.js
+++ server/lib/config/camera.js
@@ -1,6 +1,9 @@
 module.exports = {
+  DEVICE: 1,
   CAM_WIDTH: 640,
   CAM_HEIGHT: 310,
+  FPS: 5,
+  DETECTION_INTERVAL: 2,
   RESIZE_FACTOR: 2,
   MASK_IMAGE: 'Badger.png',
   OUTPUT_IMAGE: 'output.jpg',
