699d038d69549c8c83ebb7b5e962689b47e31a80
mveritym <mveritym@gmail.com>
2017-03-08 17:45:32 +0000

    Making GIFs double time

--- server/lib/config/camera.js
+++ server/lib/config/camera.js
@@ -2,10 +2,12 @@ module.exports = {
   DEVICE: 1,
   CAM_WIDTH: 640,
   CAM_HEIGHT: 310,
-  FPS: 5,
+  FPS: 12,
+  GIF_LENGTH: 1,
   DETECTION_INTERVAL: 2,
   RESIZE_FACTOR: 2,
-  MASK_IMAGE: 'Badger.png',
+  Y_OFFSET_PERCENT: 0.12,
+  MASK_IMAGE: 'V7_Badger.png',
   OUTPUT_IMAGE: 'output.jpg',
   ALGORITHM_PATH: './node_modules/opencv/data/haarcascade_frontalface_alt_tree.xml'
 };
